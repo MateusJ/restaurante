e90f3f6090f1dd01c95ad1cd1dc1e1eafbac292f
MateusJ <mateus3048@gmail.com>
2024-04-29 14:30:25 -0300

    frontInicial

--- route.js
+++ route.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.get('/categorias', function(req, resp) {
-        // resp.send(alunos);
+        document.write('<div>testetesaosfjoasifhoasifhsaofh</div>')
 });
 
 
